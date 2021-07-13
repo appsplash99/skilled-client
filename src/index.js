19c332df7f3376dfe0f02718797a791a676a2b3f
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-13 07:27:58 +0530

    updated root index.js

--- src/index.js
+++ src/index.js
@@ -1,17 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
-import { LibraryContextProvider } from './context/libraryState';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import { BrowserRouter as Router } from "react-router-dom";
+import { LibraryContextProvider } from "./context/libraryState";
+import { ToastProvider } from "./context/toastState";
+import "react-toastify/dist/ReactToastify.css";
+import "./index.css";
 
 ReactDOM.render(
   <React.StrictMode>
     <LibraryContextProvider>
-      <Router>
-        <App />
-      </Router>
+      <ToastProvider>
+        <Router>
+          <App />
+        </Router>
+      </ToastProvider>
     </LibraryContextProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
