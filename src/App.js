e2e32ffda1d916c86f8c54a2139350ec12618686
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-13 07:28:05 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,29 +1,76 @@
-import './App.css';
-import { Routes, Route } from 'react-router-dom';
-import { VideoListing } from './pages/VideoListing/VideoListing';
-import { SkilledNavbar } from './components/SkilledNavbar/SkilledNavbar';
-import { useState } from 'react';
-import { VideoPage } from './pages/VideoPage/VideoPage';
-import { PlaylistsPage } from './pages/PlaylistsPage/PlaylistsPage';
-import { WatchLaterPage } from './pages/WatchLaterPage/WatchLaterPage';
-import { LikedVideosPage } from './pages/LikedVideosPage/LikedVideos';
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
+import { LoaderCometSpinner } from "morphine-ui";
+import { PrivateRoute } from "./components/PrivateRoute";
+import { SkilledNavbar } from "./components/SkilledNavbar/SkilledNavbar";
+import { Login, SignUp, VideoDetail, VideoListing, LikedVideos, WatchLater, Playlists } from "./pages";
+import "./App.css";
+import { PLAYLIST_ROUTE, VIDEOS_URL } from "./utils/apiRoutes";
+import { getLocalCredentials } from "./utils/localStorage";
+import { loadVideosFromDB, loadUserPlaylists } from "./utils/serverRequests";
+import { useLibraryContext } from "./context/libraryState";
+import { useToast } from "./context/toastState";
 
 function App() {
   const [showMobileNav, setShowMobileNav] = useState(false);
+  const [isLoading, setIsLoading] = useState(true);
+  const { token } = getLocalCredentials();
+  const { dispatch } = useLibraryContext();
+  const { ToastContainer, toast } = useToast();
+
+
+  useEffect(() => {
+    setIsLoading(true);
+    token && loadUserPlaylists({ dispatch, url: PLAYLIST_ROUTE, token, toast });
+    setIsLoading(false);
+  }, [token]);
+
+  if (isLoading === true) {
+    return (
+      <div
+        style={{
+          height: "80vh",
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+        }}
+      >
+        <LoaderCometSpinner color="lightblue" style={{ height: "100px", width: "100px" }} />
+      </div>
+    );
+  }
+
   return (
     <div className="App">
-      <SkilledNavbar
-        setShowMobileNav={setShowMobileNav}
-        showMobileNav={showMobileNav}></SkilledNavbar>
-      <div>
+      <SkilledNavbar setShowMobileNav={setShowMobileNav} showMobileNav={showMobileNav}></SkilledNavbar>
+      <div style={{ marginTop: "7rem", paddingBottom: "7rem", overflowX: "hidden" }}>
         <Routes>
+          {/* PUBLIC ROUTES */}
           <Route path="/" element={<VideoListing />} />
-          <Route path="/:videoId" element={<VideoPage />} />
-          <Route path="/playlist" element={<PlaylistsPage />} />
-          <Route path="/watch-later" element={<WatchLaterPage />} />
-          <Route path="/liked-videos" element={<LikedVideosPage />} />
+          <Route path="videos/:_id" element={<VideoDetail />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+
+          {/* PRIVATE ROUTES */}
+          <PrivateRoute path="/playlist" element={<Playlists />} />
+          <PrivateRoute path="/watch-later" element={<WatchLater />} />
+          <PrivateRoute path="/liked-videos" element={<LikedVideos />} />
+
+          <Route path="*" element={<>Route Not Found</>} />
         </Routes>
       </div>
+      <ToastContainer
+        position="bottom-right"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={true}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss={false}
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 }
