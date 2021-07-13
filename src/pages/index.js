51b09a4cbb323bc26b2b8d0b2cb94dbd6fb5ce3f
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-13 07:27:15 +0530

    updated all pages exports

---
+++ src/pages/index.js
@@ -0,0 +1,7 @@
+export { Playlists } from './Playlists/Playlists';
+export { LikedVideos } from './LikedVideos/LikedVideos';
+export { Login } from './Login/Login';
+export { SignUp } from './SignUp/SignUp';
+export { VideoListing } from './VideoListing/VideoListing';
+export { VideoDetail } from './VideoDetail/VideoDetail';
+export { WatchLater } from './WatchLater/WatchLater';
--- src/utils/array-functions.js
+++ src/utils/array-functions.js
@@ -1,10 +1,10 @@
-export const findItemInArray = (itemsArray, videoID) => {
-  return itemsArray.find(item => item.videoId === videoID)
-}
+export const findItemInArray = (itemsArray, currVideo_id) => {
+  return itemsArray.find((item) => item._id === currVideo_id);
+};
 
 export const isVideoIdPresentinArray = (desiredArray, videoId) => {
-  return desiredArray.filter((eachVideoId) => eachVideoId === videoId).length > 0
-}
+  return desiredArray.filter((eachVideoId) => eachVideoId === videoId).length > 0;
+};
 
 export const concatNewVideoId = (itemsArray, payloadVideoId) => {
   return itemsArray.concat(payloadVideoId);
@@ -17,7 +17,26 @@ export const removeExistingVideoIdFromArray = (itemsArray, payloadVideoId) => {
 };
 
 export const isVideoIdPresentInAnyPlaylists = (playlistsArray, videoId) => {
-  return playlistsArray.filter((videoObj) => (
-    videoObj.videos.find((eachVideoId) => eachVideoId === videoId)
-  )).length > 0
-}
+  return playlistsArray.filter((videoObj) => videoObj.videos.find((eachVideoId) => eachVideoId === videoId)).length > 0;
+};
+
+export const kFormatter = (num) => {
+  return Math.abs(num) > 999
+    ? Math.sign(num) * (Math.abs(num) / 1000).toFixed(1) + "k"
+    : Math.sign(num) * Math.abs(num);
+};
+
+/** NEW ARRAY FUNCTIONS */
+export const getIdOfAPlaylist = (myPlaylist, playlistCategory) => {
+  return myPlaylist.find((library) => library.name === playlistCategory)?._id;
+};
+
+export const isVideoInPlaylist = ({ userPlaylists, playlistId, videoId }) => {
+  const currentPlaylist = userPlaylists.find((playlist) => playlist._id === playlistId);
+  if (!currentPlaylist) return false;
+  const allVideoIDsOfCurrentPlaylist = currentPlaylist.videos.map((video) => video._id);
+  return allVideoIDsOfCurrentPlaylist.includes(videoId);
+};
+
+export const playlistsContainTheVideo = ({ playlists, video_Id }) =>
+  playlists.filter((playlistObj) => playlistObj.videos.find((videoObj) => videoObj._id === video_Id)).length > 0;
