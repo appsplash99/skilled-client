4cfd486b464d995f24cba410251e2a2d922e3909
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-13 07:22:05 +0530

    added api routes

---
+++ src/utils/apiRoutes.js
@@ -0,0 +1,9 @@
+export const BASE_URL = "https://skilled-server.apurvchimralwar.repl.co/api";
+
+export const VIDEOS_URL = "https://skilled-server.apurvchimralwar.repl.co/api/videos";
+
+export const LOGIN_ROUTE = "https://skilled-server.apurvchimralwar.repl.co/api/login";
+
+export const SIGNUP_ROUTE = "https://skilled-server.apurvchimralwar.repl.co/api/register";
+
+export const PLAYLIST_ROUTE = "https://skilled-server.apurvchimralwar.repl.co/api/playlist";
