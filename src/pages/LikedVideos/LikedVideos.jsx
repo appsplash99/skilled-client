f8a49dd20f9d1aa01e4e94345b84f23d676fffec
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-13 07:23:27 +0530

    updated likedVIdeosPage

--- src/pages/LikedVideos/LikedVideos.jsx
+++ src/pages/LikedVideos/LikedVideos.jsx
@@ -1,26 +1,32 @@
-import { useLibraryContext } from '../../context/libraryState';
-import { Link } from 'react-router-dom';
-import { FaRegTrashAlt } from 'react-icons/fa';
-import { BtnIcon } from '../../components/morphine-ui';
+import { useLibraryContext } from "../../context/libraryState";
+import { Link } from "react-router-dom";
+import { FaRegTrashAlt } from "react-icons/fa";
+import { BtnIcon } from "morphine-ui";
 
-export const LikedVideosPage = () => {
+export const LikedVideos = () => {
   const {
-    state: { likedVideos },
+    state: { playlists },
     dispatch,
   } = useLibraryContext();
+
+  // TO LOAD
+  const likedVideos = playlists.filter((playlistObj) => playlistObj.name === "Liked Videos")[0].videos;
+  // console.log(JSON.stringify(likedVideos[0], null, 2));
+
   return (
     <div className="flex flex--column align-items--c justify-content--c">
       {!likedVideos && <div>why so empty....</div>}
       {likedVideos &&
-        likedVideos.map((videoId) => (
+        likedVideos.map((videoObj) => (
           <div
             className="flex align-items--c gap p--xxs m--xxs p--xs bg--secondary border-radius--md"
-            key={videoId}>
+            key={videoObj._id}
+          >
             <div>
-              <Link to={`/:${videoId}`}>
+              <Link to={`/videos/${videoObj._id}`}>
                 <img
                   alt=""
-                  src={`https://img.youtube.com/vi/${videoId}/0.jpg`}
+                  src={videoObj.video.thumbnail}
                   height="calc(4*var(--space-xl))"
                   width="calc(4*var(--space-xl))"
                   className="border-radius--md"
@@ -28,13 +34,14 @@ export const LikedVideosPage = () => {
               </Link>
               <BtnIcon
                 variant="error"
-                btnSize="lg"
-                handleOnClickProp={() =>
+                size="lg"
+                onClick={() =>
                   dispatch({
-                    type: 'REMOVE_VIDEO_FROM_LIKED_VIDEOS',
-                    payload: videoId,
+                    type: "REMOVE_VIDEO_FROM_LIKED_VIDEOS",
+                    payload: videoObj.videoId,
                   })
-                }>
+                }
+              >
                 <FaRegTrashAlt className="text--xl text--danger m0" />
               </BtnIcon>
             </div>
