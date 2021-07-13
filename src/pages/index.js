4ae9ddeb0d3e513318d06ff9eb68930a28421e91
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-13 12:58:47 +0530

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,9 @@
-export { Playlists } from './Playlists/Playlists';
-export { LikedVideos } from './LikedVideos/LikedVideos';
-export { Login } from './Login/Login';
-export { SignUp } from './SignUp/SignUp';
-export { VideoListing } from './VideoListing/VideoListing';
-export { VideoDetail } from './VideoDetail/VideoDetail';
-export { WatchLater } from './WatchLater/WatchLater';
+export { LikedVideos } from "./LikedVideos/LikedVideos";
+export { Login } from "./Login/Login";
+export { SignUp } from "./SignUp/SignUp";
+export { VideoListing } from "./VideoListing/VideoListing";
+export { VideoDetail } from "./VideoDetail/VideoDetail";
+export { WatchLater } from "./WatchLater/WatchLater";
+export { SavedVideos } from "./SavedVideos/SavedVideos";
+export { Playlists } from "./Playlists/Playlists";
+export { PlaylistVideos } from "./PlaylistVideos/PlaylistVideos";
