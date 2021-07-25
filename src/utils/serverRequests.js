a3387f5f3d6ec65d7ca3ace831ff67957762e6fc
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-25 12:15:00 +0530

    added deleteVIdeoFromPlaylist in serverRequests

--- src/utils/serverRequests.js
+++ src/utils/serverRequests.js
@@ -18,7 +18,10 @@ export const loadVideosFromDB = async ({ dispatch, url, token, toast }) => {
 export const loadUserPlaylists = async ({ dispatch, url, token, toast }) => {
   try {
     const { data, status } = await axios({ method: "GET", url, headers: { authorization: token } });
-    if (status === 200) dispatch({ type: "LOAD_ALL_PLAYLISTS", payload: data.response });
+    if (status === 200) {
+      console.log({ data });
+      dispatch({ type: "LOAD_ALL_PLAYLISTS", payload: data.response });
+    }
   } catch (error) {
     toggleToast(error, toast, "Unable to Load User Playlist");
   }
@@ -52,4 +55,23 @@ export const createNewPlaylistInDb = async ({ url, token, newPlaylistName }) => 
 
 export const deletePlaylistFromDb = async ({ url, token }) => {
   return await axios({ method: "DELETE", url, headers: { authorization: token } });
+};
+
+export const deleteVideoFromPlaylist = async ({ toast, url, token, dispatch }) => {
+  toggleToast(toast, "info", "Removing Video From Playlist...");
+  try {
+    const {
+      data: { success, response },
+    } = await axios({
+      method: "DELETE",
+      url,
+      headers: { authorization: token },
+    });
+    if (success) {
+      dispatch({ type: "LOAD_VIDEOS_OF_A_PLAYLIST", payload: response });
+      toggleToast(toast, "success", "Video Removed from Playlist");
+    }
+  } catch (error) {
+    toggleToast(toast, "error", "Unable to Remove VIdeo from Playlist");
+  }
 };
