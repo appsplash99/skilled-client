1f9d6d85786f569cc6568d983cc496466e757806
Apurv Chimralwar <apurvchimralwar1997@gmail.com>
2021-07-25 12:18:34 +0530

    added VideoCardHorizontal with deletVidFrmPlaylist

--- src/components/VideoCardHorizontal/index.jsx
+++ src/components/VideoCardHorizontal/index.jsx
@@ -1,4 +1,3 @@
-import axios from "axios";
 import { BASE_URL } from "../../utils/apiRoutes";
 import { DataBadgeIcon } from "morphine-ui";
 import { BsTrashFill } from "react-icons/bs";
@@ -6,31 +5,13 @@ import { useLibraryContext } from "../../context/libraryState";
 import { useToast } from "../../context/toastState";
 import { Link } from "react-router-dom";
 import { getLocalCredentials } from "../../utils/localStorage";
+import { deleteVideoFromPlaylist } from "../../utils/serverRequests";
 
 export const VideoCardHorizontal = ({ video, playlistId, playlistName, videoObj }) => {
   const { videoId, title, channelTitle } = video;
   const { dispatch } = useLibraryContext();
-  const { toggleToast, toast } = useToast();
+  const { toast } = useToast();
   const { token } = getLocalCredentials();
-
-  const deleteFromPlaylist = async () => {
-    toggleToast(toast, "info", "Removing Video From Playlist...");
-    try {
-      const {
-        data: { success, response },
-      } = await axios({
-        method: "DELETE",
-        url: `${BASE_URL}/playlist/${playlistId}/${video._id}`,
-        headers: { authorization: token },
-      });
-      if (success) {
-        dispatch({ type: "LOAD_VIDEOS_OF_A_PLAYLIST", payload: response });
-        toggleToast(toast, "success", "Video Removed from Playlist");
-      }
-    } catch (error) {
-      toggleToast(toast, "error", "Unable to Remove VIdeo from Playlist");
-    }
-  };
   return (
     <div
       className="flex flex--column gap border-radius--sm p"
@@ -47,7 +28,19 @@ export const VideoCardHorizontal = ({ video, playlistId, playlistName, videoObj 
         size="xl"
         variant="error"
         shape="circular"
-        icon={<BsTrashFill className="text--xl text--danger" onClick={deleteFromPlaylist} />}
+        icon={
+          <BsTrashFill
+            className="text--xl text--danger"
+            onClick={() =>
+              deleteVideoFromPlaylist({
+                url: `${BASE_URL}/playlist/${playlistId}/${video._id}`,
+                toast,
+                dispatch,
+                token,
+              })
+            }
+          />
+        }
       ></DataBadgeIcon>
     </div>
   );
